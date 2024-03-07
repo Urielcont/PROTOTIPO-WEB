66f49789dbf2577f444aca0fa963ce95bd7c910d
MiguelSotoD <23soto330@gmail.com>
2024-03-07 08:53:08 -0600

    Se subio el SideBar del admin

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './pages/login';
 import Register from './pages/registrar';
+import Sidepage from './pages/sidebar';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element ={<h2>pagina raiz</h2>}/>
             <Route path="/login" element ={<Login/>}/>
             <Route path="/registrar" element ={<Register/>}/>
+            <Route path='/Side' element={<Sidepage/>}></Route>
         </Routes>
     </BrowserRouter>
   )
