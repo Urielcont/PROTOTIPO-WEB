a5619dde2b7d428cc7bd9dfaef7900c2cc55b81e
Gael Emmanuel Valles Yañez <91341325+GaelValles@users.noreply.github.com>
2024-03-09 21:51:17 -0600

    creacion del token

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,13 +1,11 @@
 const {User} = require("../model/user.model");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { CreateAccessToken } = require("../libs/jwt");
 // Registrar un nuevo usuario
 exports.registrar= async (req,res)=>{
    
         const { nombres, telefono, correo, password} = req.body;
         try {
-            
-
         const userFound = await User.findOne({correo});
         if (userFound){
             return res.status(400).json(["la cuenta ya esta en uso"]);
@@ -15,35 +13,19 @@ exports.registrar= async (req,res)=>{
         const passwordHash= await bcrypt.hash(password,10);
             // Crear el usuario 
         const usuario = new User({
-            nombres,
+            nombres,  
             telefono,
             correo,
             password:passwordHash,
         });
         await usuario.save();
-
-        jwt.sign({
-            id:usuario._id,
-        }
-        ,
-        "encriptado123",
-        {
-            expiresIn:"1d",
-        },
-        (error, token) => {
-            if (error) {
-              console.log(error);
-              res.status(500).json({ message: 'Error interno del servidor' });
-            } else {
-              res.cookie('token',token);
-              res.json({
-                message:"usuario creado correctamente",
-              })
-            }
-          }      
-        )
+        const token = await CreateAccessToken({id:usuario._id});
+        res.cookie('token',token);
+        res.json({
+          message:"usuario creado correctamente",
+        })
     } catch (error) {
-     console.log(error)       
+      res.status(500).json({message:error.message})      
     }
 
 }
