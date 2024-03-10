d50550c525b36b29c937c4fa39993ed1e6d3136c
MiguelSotoD <23soto330@gmail.com>
2024-03-10 12:15:55 -0600

    Se subio de nuevo

--- FrontEnd/src/pages/perfil.jsx
+++ FrontEnd/src/pages/perfil.jsx
@@ -19,6 +19,34 @@ function PerfilPage() {
             </div>
         </div>
 
+        <form className="mt-16 w-96 ml-96 border rounded py-5 px-5 text-gray-700 shadow mx-auto">
+    <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-" htmlFor="nombre">
+            Nombre(s):
+        </label>
+        <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="nombre"
+            type="text"
+            placeholder="Nombre(s)"
+        />
+    </div>
+    <div className="flex items-center justify-between">
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+        >
+            Guardar
+        </button>
+        <button
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+        >
+            Cancelar
+        </button>
+    </div>
+</form>
+
 
         </div>
         
