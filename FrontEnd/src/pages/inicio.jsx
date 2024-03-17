734258f89b03045a12d64ea8be59cb2219e25b5c
Urielcont <Urielalvaro3@gmail.com>
2024-03-16 21:43:39 -0600

    Corregir detalles de pagina inicio

--- FrontEnd/src/pages/inicio.jsx
+++ FrontEnd/src/pages/inicio.jsx
@@ -48,72 +48,47 @@ function InicioPage() {
 
     return (
         <div className="m-0">
-            <h1 className="mr-96 text-xl flex justify-center" >Resumen del Dia</h1>
-            <hr className="my-2 text-black" />
-            <div>
-                <SidePage />
-            </div>
+        <h1 className="mr-56 text-xl" >Resumen del Dia</h1>
+        <hr className="my-2 text-black" />
+        <div>
+            <SidePage />
+        </div>
 
-            <div className="mt-1 flex justify-center ">
+        <div className="mt-1 flex justify-center ">
 
-                <div className="ml-60">
-                    {/* Seccion del ph */
-
-                    <div className="ml-10 mt-10 bg-customBlue h-52 w-80 m-6 rounded-md">
-                        <i className="bi-droplet-half ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
+            <div className="ml-60">
+                {/* Seccion del ph */
+                    <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
+                        <i className="bi-droplet-half ml-28" style={{ fontSize: '2.5rem' }}></i>
+                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelPh}</div>
                         <p className="text-zinc-950 mt-2">Nivel del PH del Agua</p>
                     </div>}
-                    {/* Seccion del flujo del agua */
-                    <div className="ml-10 mt-16 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-water ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
+                {/* Seccion del flujo del agua */
+                    <div className="ml-10 mt-16 bg-customBlue h-40 w-64 m-6 rounded-md">
+                        <i className="bi-water ml-28" style={{ fontSize: '2.5rem' }}></i>
+                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelFlujo}</div>
                         <p className="text-zinc-950 mt-2">Flujo del Agua</p>
                     </div>}
 
-                        <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
-                            <i className="bi-droplet-half ml-28" style={{ fontSize: '2.5rem' }}></i>
-                            <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelPh}</div>
-                            <p className="text-zinc-950 mt-2">Nivel del PH del Agua</p>
-                        </div>
-                    {/* Seccion del flujo del agua */
-                        <div className="ml-10 mt-16 bg-customBlue h-40 w-64 m-6 rounded-md">
-                            <i className="bi-water ml-28" style={{ fontSize: '2.5rem' }}></i>
-                            <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelFlujo}</div>
-                            <p className="text-zinc-950 mt-2">Flujo del Agua</p>
-                        </div>}
-
-                </div>
-                <div>
-                    {/* Seccion de las ventas */
-
-                    <div className="ml-10 mt-10 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-cash-coin ml-28"style={{ fontSize: '2.5rem' }}></i>
+            </div>
+            <div>
+                {/* Seccion de las ventas */
+                    <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
+                        <i className="bi-cash-coin ml-28" style={{ fontSize: '2.5rem' }}></i>
                         <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
                         <p className="text-zinc-950 mt-2">Total de Ventas</p>
                     </div>}
-                    {/* Seccion de la turbidez */
-                    <div className="ml-10 mt-16 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-tsunami ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
+                {/* Seccion de la turbidez */
+                    <div className="ml-10 mt-16 bg-customBlue h-40 w-64 m-6 rounded-md">
+                        <i className="bi-tsunami ml-28" style={{ fontSize: '2.5rem' }}></i>
+                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelTurbidez}</div>
                         <p className="text-zinc-950 mt-2">Turbidez del Agua</p>
                     </div>}
-                        <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
-                            <i className="bi-cash-coin ml-28" style={{ fontSize: '2.5rem' }}></i>
-                            <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
-                            <p className="text-zinc-950 mt-2">Total de Ventas</p>
-                        </div>
-                    {/* Seccion de la turbidez */
-                        <div className="ml-10 mt-16 bg-customBlue h-40 w-64 m-6 rounded-md">
-                            <i className="bi-tsunami ml-28" style={{ fontSize: '2.5rem' }}></i>
-                            <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelTurbidez}</div>
-                            <p className="text-zinc-950 mt-2">Turbidez del Agua</p>
-                        </div>}
-                </div>
             </div>
-
-
         </div>
+
+
+    </div>
 
     );
 }
