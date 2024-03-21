161e9accd5797ca422171be87989f22803f6b39b
MiguelSotoD <23soto330@gmail.com>
2024-03-21 11:48:09 -0600

    Se termino la interfaz de Flujo

--- FrontEnd/src/pages/flujo.jsx
+++ FrontEnd/src/pages/flujo.jsx
@@ -9,17 +9,31 @@ function FlujoPage() {
             <SidePage />
             </div>
 
-            <div className="mt-1 flex justify-center">
-                <div className="mt-24 ml-60 h-56 w-2/6 bg-black rounded-xl">
-                <div>
-                    <div className="mt-8 ml-auto mr-auto bg-sky-400 h-40 w-96  rounded-xl flex">
-                        <div className="ml-auto mr-auto bg-lime-300 h-32 w-80 rounded-xl mt-3">
-                            <h1 className="text-3xl mt-4">0.000 L</h1>
-                            <h1>0.0</h1>
-                            <h1>Reset total</h1>
-                        </div>
+    <div className="mt-1 flex justify-center">
+        <div className="mt-24 ml-60 h-56 w-2/6 bg-gray-400 rounded-xl">
+            <div className="mt-10 mx-auto my-auto bg-stone-950 h-32 w-80 rounded-xl">
+                <h1 className="font-semibold text-5xl mt-4 m-auto text-center text-red-600 mb-3">0.000 L</h1>
+                <h1 className=" font-medium text-center text-lime-500 mb-2 text-xl">0.0</h1>
+                <h1 className="text-center text-white ">Reset total</h1>
+            </div>
+        </div>
+    </div>
+
+            <div className="flex justify-center mb-10">
+                <div className="mt-14 w-5/12 ml-60 border rounded-2xl py-10 px-1 shadow ">
+                    <h1 className="text-center text-xl mb-3">Historial de reviciones</h1>
+                    <hr className="w-full" />
+                    <div className="mt-4 flex justify-between mb-3">
+                        <h2 className="ml-6">Fecha </h2>
+                        <h2>Nivel de PH</h2>
+                        <h2 className="mr-6">Estado</h2>
                     </div>
-                </div>
+                    <hr />
+                    <div className="mt-4 flex justify-between mb-3">
+                        <h2>21/03/2024</h2>
+                        <h2>666</h2>
+                        <h2 >Base</h2>
+                    </div>
                 </div>
             </div>
         </div>
--- FrontEnd/src/pages/inicio.jsx
+++ FrontEnd/src/pages/inicio.jsx
@@ -58,19 +58,6 @@ function InicioPage() {
 
                 <div className="ml-60">
                     {/* Seccion del ph */
-<<<<<<< HEAD
-                    <div className="ml-10 mt-10 bg-customBlue h-52 w-80 m-6 rounded-md">
-                        <i className="bi-droplet-half ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
-                        <p className="text-zinc-950 mt-2">Nivel del PH del Agua</p>
-                    </div>}
-                    {/* Seccion del flujo del agua */
-                    <div className="ml-10 mt-16 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-water ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
-                        <p className="text-zinc-950 mt-2">Flujo del Agua</p>
-                    </div>}
-=======
                         <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
                             <i className="bi-droplet-half ml-28" style={{ fontSize: '2.5rem' }}></i>
                             <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelPh}</div>
@@ -82,24 +69,10 @@ function InicioPage() {
                             <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelFlujo}</div>
                             <p className="text-zinc-950 mt-2">Flujo del Agua</p>
                         </div>}
->>>>>>> b65a3a76d57bacaa554b8ca5e7028a143ded3a6b
 
                 </div>
                 <div>
                     {/* Seccion de las ventas */
-<<<<<<< HEAD
-                    <div className="ml-10 mt-10 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-cash-coin ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
-                        <p className="text-zinc-950 mt-2">Total de Ventas</p>
-                    </div>}
-                    {/* Seccion de la turbidez */
-                    <div className="ml-10 mt-16 bg-customBlue h-52 w-80 m-6 rounded-md">
-                    <i className="bi-tsunami ml-28"style={{ fontSize: '2.5rem' }}></i>
-                        <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
-                        <p className="text-zinc-950 mt-2">Turbidez del Agua</p>
-                    </div>}
-=======
                         <div className="ml-10 mt-10 bg-customBlue h-40 w-64 m-6 rounded-md">
                             <i className="bi-cash-coin ml-28" style={{ fontSize: '2.5rem' }}></i>
                             <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">7</div>
@@ -111,7 +84,6 @@ function InicioPage() {
                             <div className="bg-customBlue2 h-8 w-20 rounded-md ml-7">{nivelTurbidez}</div>
                             <p className="text-zinc-950 mt-2">Turbidez del Agua</p>
                         </div>}
->>>>>>> b65a3a76d57bacaa554b8ca5e7028a143ded3a6b
                 </div>
             </div>
 
