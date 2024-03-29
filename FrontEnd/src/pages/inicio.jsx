6dff24c990afa706198b72be47b3e21b4cd5fdc9
MiguelSotoD <23soto330@gmail.com>
2024-03-28 21:14:03 -0600

    Retoque de estilos

--- FrontEnd/src/pages/inicio.jsx
+++ FrontEnd/src/pages/inicio.jsx
@@ -60,7 +60,7 @@ function InicioPage() {
             {/* Seccion del ph */
             <div className="bg-[url('src/assets/images/inicio_img1.png')] w-[300px] h-[708px] justify-center flex flex-col items-center">
                 <h1 className="text-3xl font-semibold text-center mb-3">Nivel del agua</h1>
-                <div className="bg-white shadow-xl shadow-white hover:shadow-customBlue3 h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
+                <div className="bg-white shadow-xl shadow-customBlue3 hover:shadow-white h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
                     <i className="bi-droplet-half" style={{ fontSize: '3.2rem' }}></i>
                     <div className="bg-customBlue3 h-1/4 w-4/6 rounded-full mt-2 text-center text-xl">{nivelPh}</div>
                 </div>
@@ -68,7 +68,7 @@ function InicioPage() {
             {/* Seccion de la turbidez */
             <div className="bg-[url('src/assets/images/inicio_img2.png')] w-[300px] h-[708px] justify-center flex flex-col items-center">
             <h1 className="text-3xl font-semibold text-center mb-3">Turbidez del agua</h1>
-            <div className="bg-white shadow-xl shadow-white hover:shadow-customBlue3 h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
+            <div className="bg-white shadow-xl shadow-customBlue3 hover:shadow-white h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
                 <i className="bi-tsunami" style={{ fontSize: '3.2rem' }}></i>
                 <div className="bg-customBlue3 h-1/4 w-4/6 rounded-full mt-2 text-center text-xl">{nivelTurbidez}</div>
             </div>
@@ -76,7 +76,7 @@ function InicioPage() {
             {/* Seccion del flujo del agua */
             <div className="bg-[url('src/assets/images/inicio_img3.png')] w-[300px] h-[708px] justify-center flex flex-col items-center">
                 <h1 className="text-3xl font-semibold text-center mb-3">Flujo del agua</h1>
-                <div className="bg-white shadow-xl shadow-white hover:shadow-customBlue3 h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
+                <div className="bg-white shadow-xl shadow-customBlue3 hover:shadow-white h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
                     <i className="bi-water" style={{ fontSize: '3.2rem' }}></i>
                     <div className="bg-customBlue3 h-1/4 w-4/6 rounded-full mt-2 text-center text-xl">{nivelFlujo}</div>
                 </div>
@@ -84,7 +84,7 @@ function InicioPage() {
             {/* Seccion de las ventas */
             <div className="bg-[url('src/assets/images/inicio_img4.png')] w-[300px] h-[708px] justify-center flex flex-col items-center">
                 <h1 className="text-3xl font-semibold text-center mb-3">Total de ventas</h1>
-                <div className="bg-white shadow-xl shadow-white hover:shadow-customBlue3 h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
+                <div className="bg-white shadow-xl shadow-customBlue3 hover:shadow-white h-40 w-6/12 m-6 rounded-[50px] content-center flex flex-col items-center">
                     <i className="bi-cash-coin" style={{ fontSize: '3.2rem' }}></i>
                     <div className="bg-customBlue3 h-1/4 w-4/6 rounded-full mt-2 text-center text-xl">$1,202</div>
                 </div>
