ee520a1118492ff2ff3d84dacdd1915816be41ff
Urielcont <Urielalvaro3@gmail.com>
2024-04-09 21:13:12 -0600

    Quitar la recarga de cada pagina

--- FrontEnd/src/pages/sidebar.jsx
+++ FrontEnd/src/pages/sidebar.jsx
@@ -1,9 +1,9 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import React, { useState } from 'react';
+import { useState, } from 'react';
 import Logo from "../assets/images/logo.png";
 import { useAuth } from '../context/Auth.context';
 import Swal from 'sweetalert2';
-
+import { Link } from 'react-router-dom';
 
 function Sidepage() {
   const {logout, user}= useAuth();
@@ -20,7 +20,7 @@ function Sidepage() {
   const handleLogout = () => {
     Swal.fire({
       title: 'Seguro de salir?',
-      text: "Estás a punto de cerrar sesión",
+      text: "Estás Link punto de cerrar sesión",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -42,42 +42,42 @@ function Sidepage() {
       <div className={`sidebar fixed top-0 bottom-0 left-0 lg:left-0 duration-1000 p-2 w-[300px] overflow-y-auto text-center bg-white shadow h-screen`}>
         <div className="text-black text-xl">
           <div className="p-2.5 mt-1 flex items-center rounded-md">
-            <a href='/Inicio'>
+            <Link to='/Inicio'>
               <img className="w-16" src={Logo} alt="" />
-            </a>
+            </Link>
             <i className="bi bi-x ml-44 cursor-pointer lg:hidden" onClick={toggleSidebar}style={{ fontSize: '2rem' }}></i>
           </div>
 
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer ">
-            <a href="/Inicio"><i className="bi bi-house-door-fill hover:text-blue-700"></i></a>
-            <a href="/Inicio"><h1 className="text-[17px] ml-4 text-black hover:text-blue-700">Inicio</h1></a>
+            <Link to="/Inicio"><i className="bi bi-house-door-fill hover:text-blue-700"></i></Link>
+            <Link to="/Inicio"><h1 className="text-[17px] ml-4 text-black hover:text-blue-700">Inicio</h1></Link>
           </div>
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/Calidad"><i className="bi bi-droplet-half hover:text-blue-700"></i></a>
-            <a href="/Calidad"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Calidad</span></a>
+            <Link to="/Calidad"><i className="bi bi-droplet-half hover:text-blue-700"></i></Link>
+            <Link to="/Calidad"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Calidad</span></Link>
           </div>
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/Ph"><i className="bi bi-moisture hover:text-blue-700"></i></a>
-            <a href="/Ph"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Ph</span></a>
+            <Link to="/Ph"><i className="bi bi-moisture hover:text-blue-700"></i></Link>
+            <Link to="/Ph"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Ph</span></Link>
           </div>
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/Flujo"><i className="bi bi-arrow-left-right hover:text-blue-700 "></i></a>
-            <a href="/Flujo"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Flujo</span></a>
+            <Link to="/Flujo"><i className="bi bi-arrow-left-right hover:text-blue-700 "></i></Link>
+            <Link to="/Flujo"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Flujo</span></Link>
           </div>
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/Ventas"><i className="bi bi-graph-up-arrow hover:text-blue-700"></i></a>
-            <a href="/Ventas"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Ventas</span></a>
+            <Link to="/Ventas"><i className="bi bi-graph-up-arrow hover:text-blue-700"></i></Link>
+            <Link to="/Ventas"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Ventas</span></Link>
           </div>
           <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/Perfil"><i className="bi bi-person-circle hover:text-blue-700"></i></a>
-            <a href="/Perfil"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Perfil</span></a>
+            <Link to="/Perfil"><i className="bi bi-person-circle hover:text-blue-700"></i></Link>
+            <Link to="/Perfil"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Perfil</span></Link>
           </div>
           {user.rol === true ? 
           (
           <>
             <div className="p-2 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer">
-            <a href="/user"><i className="bi bi-people  hover:text-blue-700"></i></a>
-            <a href="/user"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Usuarios</span></a>
+            <Link to="/user"><i className="bi bi-people  hover:text-blue-700"></i></Link>
+            <Link to="/user"><span className="text-[17px] ml-4 text-black hover:text-blue-700">Usuarios</span></Link>
             </div>
           </>
           ):(
