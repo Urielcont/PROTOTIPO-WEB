a9ecc419cbd3affa67fccfe4bc4dc0e6b8e5f78d
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2024-04-21 23:14:44 -0600

    cambios del context

--- FrontEnd/src/context/sensores.context.jsx
+++ FrontEnd/src/context/sensores.context.jsx
@@ -13,97 +13,109 @@ export const SensorProvider = ({ children }) => {
 
   const [historialFlujo, setHistorialFlujo] = useState([]);
   const [historialPh, setHistorialPh] = useState([]);
-  const [historialVentas, sethistorialVentas]=useState([]);
-  const [historialCalidad, sethistorialCalidad]=useState([]);
+  const [historialVentas, sethistorialVentas] = useState([]);
+  const [historialCalidad, sethistorialCalidad] = useState([]);
 
   const [nivelPh, setnivelPh] = useState([]); //Ultimo valor de ph en la base de datos
   const [nivelFlujo, setnivelFlujo] = useState([]);//Ultimo valor de Flujo en la base de datos
   const [nivelTurbidez, setnivelTurbidez] = useState([]);//Ultimo valor de Turbidez en la base de datos
-  const [ultimaVenta, setultimaVenta]=useState([]);
-  const [totalVentas, settotalVentas]=useState([]);
-  const [totalSemanaVentas, settotalSemanaVentas]=useState([]);
-  const [totalMesVentas, settotalMesVentas]=useState([]);
+  const [ultimaVenta, setultimaVenta] = useState([]);
+  const [totalVentas, settotalVentas] = useState([]);
+  const [totalSemanaVentas, settotalSemanaVentas] = useState([]);
+  const [totalMesVentas, settotalMesVentas] = useState([]);
 
 
   // variable usada para conectar a la api
   const api = 'http://localhost:4000/api'
   // -------OBTENER ULTIMO DATO DE LA BASE DE DATOS DE SENSORES----
   useEffect(() => {
-    MostrarFlujo();
-    obtenerUltimoDato();
-    MostrarPh();
-    MostrarVentas();
+    // MostrarFlujo();
+    // obtenerUltimoDato();
+    // MostrarPh();
+    // MostrarVentas();
   }, []);
-  const obtenerUltimoDato = async () => {
-    try {
-      // Hacer solicitudes HTTP para obtener los datos más recientes
-      const responsePh = await axios.get(`${api}/ph`);
-      const responseFlujo = await axios.get(`${api}/flujo`);
-      const responseCalidad = await axios.get(`${api}/MostrarCalidad`);
-      const responseTurbidez = await axios.get(`${api}/turbidez`);
-      const responseVenta = await axios.get(`${api}/UltimaVenta`);
-      const responsetotalVentas = await axios.get(`${api}/TotalVentas`);
-      const responsetotalSemanaVentas = await axios.get(`${api}/TotalSemanaVentas`);
-      const responsetotalMesVentas = await axios.get(`${api}/TotalMesVentas`);
+  try {
+    useEffect(() => {
+      const obtenerUltimoDato = async () => {
+        try {
+          // Hacer solicitudes HTTP para obtener los datos más recientes
+          const responsePh = await axios.get(`${api}/ph`);
+          const responseFlujo = await axios.get(`${api}/flujo`);
+          const responseCalidad = await axios.get(`${api}/MostrarCalidad`);
+          const responseTurbidez = await axios.get(`${api}/turbidez`);
+          const responseVenta = await axios.get(`${api}/UltimaVenta`);
+          const responsetotalVentas = await axios.get(`${api}/TotalVentas`);
+          const responsetotalSemanaVentas = await axios.get(`${api}/TotalSemanaVentas`);
+          const responsetotalMesVentas = await axios.get(`${api}/TotalMesVentas`);
 
-      // Formatear la fecha de los datos
-      const datosPh = {
-        ...responsePh.data,
-        fecha: new Date(responsePh.data.fecha).toLocaleString()
+          // Formatear la fecha de los datos
+          const datosPh = {
+            ...responsePh.data,
+            fecha: new Date(responsePh.data.fecha).toLocaleString()
+          };
+          const datosFlujo = {
+            ...responseFlujo.data,
+            fecha: new Date(responseFlujo.data.fecha).toLocaleString()
+          };
+          const datosTurbidez = {
+            ...responseTurbidez.data,
+            fecha: new Date(responseTurbidez.data.fecha).toLocaleString()
+          };
+          setnivelPh(datosPh);
+          setnivelFlujo(datosFlujo);
+          setnivelTurbidez(datosTurbidez);
+          sethistorialCalidad(responseCalidad.data)
+          setultimaVenta(responseVenta.data)
+          settotalVentas(responsetotalVentas.data.total_ventas)
+          settotalSemanaVentas(responsetotalSemanaVentas.data.total_ventas_semana)
+          settotalMesVentas(responsetotalMesVentas.data.total_ventas_mes)
+          // console.log("venta sem",responsetotalSemanaVentas.data.total_ventas_semana)
+          // console.log(responsetotalMesVentas.data.total_ventas_mes)
+
+
+          // Obtener todos los datos de la base de datos
+          // const MostrarFlujo = async () => {
+          try {
+            const response = await axios.get(`${api}/mostrarFlujo`);
+            setHistorialFlujo(response.data);
+          } catch (error) {
+            console.error("Error Mostrar Flujo:", error);
+          }
+          // };
+          // const MostrarPh = async () => {
+          try {
+            const response = await axios.get(`${api}/MostrarPh`);
+            setHistorialPh(response.data);
+          } catch (error) {
+            console.error("Error Mostrar Ph:", error);
+          }
+          // };
+
+          // const MostrarVentas = async () => {
+          try {
+            const response = await axios.get(`${api}/Ventas`);
+            sethistorialVentas(response.data);
+          } catch (error) {
+            console.error("Error Mostrar las Ventas:", error);
+          }
+          // };
+        } catch (error) {
+          console.error("Error al obtener los datos:", error);
+        }
       };
-      const datosFlujo = {
-        ...responseFlujo.data,
-        fecha: new Date(responseFlujo.data.fecha).toLocaleString()
-      };
-      const datosTurbidez = {
-        ...responseTurbidez.data,
-        fecha: new Date(responseTurbidez.data.fecha).toLocaleString()
-      };
-      setnivelPh(datosPh);
-      setnivelFlujo(datosFlujo);
-      setnivelTurbidez(datosTurbidez);
-      sethistorialCalidad(responseCalidad.data)
-      setultimaVenta(responseVenta.data)
-      settotalVentas(responsetotalVentas.data.total_ventas)
-      settotalSemanaVentas(responsetotalSemanaVentas.data.total_ventas_semana)
-      settotalMesVentas(responsetotalMesVentas.data.total_ventas_mes)
-      // console.log("venta sem",responsetotalSemanaVentas.data.total_ventas_semana)
-      // console.log(responsetotalMesVentas.data.total_ventas_mes)
+      obtenerUltimoDato();
+      const interval = setInterval(obtenerUltimoDato, 1000);
 
-    } catch (error) {
-      console.error("Error al obtener los datos:", error);
-    }
-  };
+      return () => clearInterval(interval);
+    }, []);
+  } catch (error) {
+    console.log("Error al llamar los datos de los ultimos Dato", error)
+  }
 
-  // Obtener todos los datos de la base de datos
-  const MostrarFlujo = async () => {
-    try {
-      const response = await axios.get(`${api}/mostrarFlujo`);
-      setHistorialFlujo(response.data);
-    } catch (error) {
-      console.error("Error Mostrar Flujo:", error);
-    }
-  };
-  const MostrarPh = async () => {
-    try {
-      const response = await axios.get(`${api}/MostrarPh`);
-      setHistorialPh(response.data);
-    } catch (error) {
-      console.error("Error Mostrar Ph:", error);
-    }
-  };
 
-  const MostrarVentas = async () => {
-    try {
-      const response = await axios.get(`${api}/Ventas`);
-      sethistorialVentas(response.data);
-    } catch (error) {
-      console.error("Error Mostrar las Ventas:", error);
-    }
-  };
-  
-  
-  
+
+
+
 
 
   return (
